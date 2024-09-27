2239a7d2ac0762ab6b7e7a7a99ec65e77d19c9f0
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2024-09-27 14:47:29 -0400

    feat: :sparkles: finishing bot

--- src/ai.ts
+++ src/ai.ts
@@ -1,11 +1,70 @@
-import { openai } from '@ai-sdk/openai';
-import { generateText } from 'ai';
+import { generateText, type LanguageModelV1 } from 'ai';
 
-export async function generateAReview(prompt: string) {
+async function generateAReview(prompt: string, model: LanguageModelV1) {
   const { text } = await generateText({
-    model: openai('gpt-4o'),
-    system: '',
+    model,
+    system: `You are an expert code reviewer assisting software engineers in improving code quality. Your focus is to:
+        1. Spot potential issues (e.g., bugs, inefficiencies, security vulnerabilities).
+        2. Suggest improvements based on coding best practices.
+        3. Ensure readability and maintainability of the code.
+        4. Check for consistency with existing project standards and styles.
+      Your feedback should be clear, concise, and actionable to help engineers merge pull requests faster.`,
     prompt,
   });
   return text;
+}
+
+type CodeReviewParams = {
+  diff: string;
+  model: LanguageModelV1;
+};
+
+export function generateGeneralCodeReview({ diff, model }: CodeReviewParams) {
+  return generateAReview(
+    `Analyze the following code changes in this pull request. Highlight potential issues, inconsistencies, or anti-patterns in coding style, structure, or logic. Provide suggestions for improvements.
+  Here is the code diff: ${diff}`,
+    model
+  );
+}
+
+export function generatePerformanceCodeReview({
+  diff,
+  model,
+}: CodeReviewParams) {
+  return generateAReview(
+    `Examine the code changes for performance or efficiency improvements. Are there any potential optimizations, memory concerns, or slow operations that could be optimized?
+    Consider the following code diff: ${diff}`,
+    model
+  );
+}
+
+export function generateSecurityCodeReview({ diff, model }: CodeReviewParams) {
+  return generateAReview(
+    `Review the code changes for any security vulnerabilities or practices that might introduce risk. Highlight any potential security flaws related to data handling, user input, authentication, or API exposure. Consider the following code diff: ${diff}`,
+    model
+  );
+}
+
+export function generateMaintainabilityCodeReview({
+  diff,
+  model,
+}: CodeReviewParams) {
+  return generateAReview(
+    `Assess the following code changes for readability and maintainability. How could the code be structured to be easier to understand and maintain over time? Are there opportunities to simplify or clarify? Consider the following code diff: ${diff}`,
+    model
+  );
+}
+
+export function generateWalkthrough({ diff, model }: CodeReviewParams) {
+  return generateAReview(
+    `Create a able that have been changed in this pull request. For each file, briefly explain what type of change was made (e.g., new functionality, refactoring, bug fix) and how it fits into the overall context of the PR. Consider the following code diff: ${diff}`,
+    model
+  );
+}
+
+export function generateIssueValidation({ diff, model }: CodeReviewParams) {
+  return generateAReview(
+    `Given the following code changes, is this pull request ready to merge? Summarize any critical issues that should be addressed before merging, and highlight any sections that may require further review. Consider the following code diff: ${diff}`,
+    model
+  );
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import * as core from '@actions/core';
-import { wait } from './wait';
+import * as github from '@actions/github';
+import { createOpenAI } from '@ai-sdk/openai';
+import {
+  generateGeneralCodeReview,
+  generatePerformanceCodeReview,
+  generateSecurityCodeReview,
+  generateMaintainabilityCodeReview,
+  generateWalkthrough,
+  generateIssueValidation,
+} from './ai';
 
 /**
  * The main function for the action.
@@ -7,18 +16,79 @@ import { wait } from './wait';
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
+    const OPEN_AI_KEY = core.getInput('OPEN_AI_KEY', {
+      required: true,
+    });
+    const token = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(token);
+    const model = createOpenAI({
+      apiKey: OPEN_AI_KEY,
+    }).languageModel('gpt-4o');
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`);
+    const { owner, repo } = github.context.repo;
+    const pull_number = github.context.payload.pull_request?.number;
 
-    // Log the current timestamp, wait, then log the new timestamp
+    if (typeof pull_number === 'undefined') {
+      core.error('TypeError pull_number var is undefined');
+      return;
+    }
+
+    const { data: pullRequest } = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number,
+      mediaType: {
+        format: 'diff',
+      },
+    });
+    const diff = pullRequest.body;
+
+    if (typeof diff === 'undefined' || diff === null) {
+      core.error('TypeError diff var is undefined');
+      return;
+    }
+    const [
+      generalCodeReview,
+      performanceCodeReview,
+      securityCodeReview,
+      maintainabilityCodeReview,
+      walkthrough,
+      issueValidation,
+    ] = await Promise.allSettled([
+      generateGeneralCodeReview({ diff, model }),
+      generatePerformanceCodeReview({ diff, model }),
+      generateSecurityCodeReview({ diff, model }),
+      generateMaintainabilityCodeReview({ diff, model }),
+      generateWalkthrough({ diff, model }),
+      generateIssueValidation({ diff, model }),
+    ]);
     core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString());
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pull_number,
+      body: `# Automatic code review for #${pull_number}
+
+      ## Description
+      ${generalCodeReview}
+
+      ## Walkthrough
+      ${walkthrough}
+
+      ## Perfomance
+      ${performanceCodeReview}
+
+      ## Security
+      ${securityCodeReview}
+
+      ## Maintainability
+      ${maintainabilityCodeReview}
+
+      ## Is mergeable?
+      ${issueValidation}
+      `,
+    });
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message);
